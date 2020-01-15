0ede53a779504edf1cbc03e3199d81e77376cd96
Pedro M <membrep@gmail.com>
2020-01-15 12:41:00 -0800

    updated content on landing page

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -95,7 +95,7 @@ function designForm() {
 $(document).ready(function() {
 	Typed.new("#writing-text", {
 		strings: [
-			"am a Full-Stack Web Developer.", "love code.", "also teach programming to people.", "solve problems."
+			"I am a Full-Stack Web Developer.", "I teach programming to people.", "I solve problems."
 		],
 		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 		stringsElement: null,
